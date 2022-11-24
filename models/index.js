2a015fa76de7a9bce531bca4dcb1d8ad4cce4aeb
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-11-24 16:57:15 +0200

    static files + config files

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const {DB_CONFIG} = require('../config')
+const config = require(DB_CONFIG)[env];
 const db = {};
 
 let sequelize;
--- routes/group-router.js
+++ routes/group-router.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const GroupController = require('../controllers/Group.controller')
 const multer = require('multer')
+const {STATIC_PATH} = require('../config')
 const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, '../public/images'))
+      cb(null, STATIC_PATH)
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
